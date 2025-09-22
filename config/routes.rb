cbdf643112e239b14e6512301cc85ed08d2c7bb5
root <root@localhost>
2025-09-22 02:12:41 +0000

    login view and student dashboard view

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,16 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root to: 'dashboards#show'
+  
+  # Role-specific dashboard routes
+  get 'student_dashboard', to: 'dashboards#student', as: :student_dashboard
+  get 'advisor_dashboard', to: 'dashboards#advisor', as: :advisor_dashboard
+  
+  devise_for :admins, controllers: { omniauth_callbacks: 'admins/omniauth_callbacks' }
+  
+  devise_scope :admin do
+    get 'admins/sign_in', to: 'admins/sessions#new', as: :new_admin_session
+    get 'admins/sign_out', to: 'admins/sessions#destroy', as: :destroy_admin_session
+  end
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.

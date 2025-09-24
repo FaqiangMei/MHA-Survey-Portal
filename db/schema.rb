2752ef31d6f3bd892ec3de9aa19cccb5bf9223f7
root <root@localhost>
2025-09-24 22:21:27 +0000

    Fixed names

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_09_24_154224) do
+ActiveRecord::Schema[8.0].define(version: 2025_09_24_213122) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -91,7 +91,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_09_24_154224) do
     t.integer "student_id"
     t.string "name"
     t.string "email"
-    t.string "NetID"
+    t.string "net_id"
     t.integer "track"
     t.integer "advisor_id"
     t.datetime "created_at", null: false
